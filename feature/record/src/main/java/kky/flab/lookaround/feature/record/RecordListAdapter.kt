e902b837d983a8eacbbd9b4481dce5e768f47051
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-08-06 22:00:04 +0900

    feat : 산책 기록 탭 구현
    - UI 구현
    - Repository 연결

---
+++ feature/record/src/main/java/kky/flab/lookaround/feature/record/RecordListAdapter.kt
@@ -0,0 +1,62 @@
+package kky.flab.lookaround.feature.record
+
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import kky.flab.lookaround.core.domain.model.Record
+import kky.flab.lookaround.core.ui.util.millsToTimeFormat
+import kky.flab.lookaround.feature.record.databinding.ItemRecordBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class RecordListAdapter :
+    ListAdapter<Record, RecordListAdapter.RecordViewHolder>(RecordDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val binding = ItemRecordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class RecordViewHolder(val binding: ItemRecordBinding) : ViewHolder(binding.root) {
+        fun onBind(item: Record) {
+            val date = Date(item.startTimeStamp)
+            val sdf = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+            binding.tvDate.text = sdf.format(date)
+            binding.tvTime.text = (item.endTimeStamp - item.startTimeStamp).millsToTimeFormat()
+            binding.tvDistance.text = "${item.distance}m"
+            binding.tvMemo.text = item.memo
+
+            val hasImage = item.imageUri.isNotEmpty()
+            binding.ivPhoto.isVisible = hasImage
+            if (hasImage) {
+                val bitmap = ImageDecoder.decodeBitmap(
+                    ImageDecoder.createSource(
+                        binding.root.context.contentResolver,
+                        Uri.parse(item.imageUri)
+                    )
+                )
+                binding.ivPhoto.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private class RecordDiffUtil : DiffUtil.ItemCallback<Record>() {
+        override fun areItemsTheSame(oldItem: Record, newItem: Record): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Record, newItem: Record): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
