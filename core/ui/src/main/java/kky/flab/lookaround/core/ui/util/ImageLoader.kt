ec4a9283b80184461bdae89a43eb0124f55b289b
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-08-26 21:14:28 +0900

    refactor : 이미지 로딩 시 viewLifecycle에 맞출 수 있도록 수정

--- core/ui/src/main/java/kky/flab/lookaround/core/ui/util/ImageLoader.kt
+++ core/ui/src/main/java/kky/flab/lookaround/core/ui/util/ImageLoader.kt
@@ -5,14 +5,15 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.util.LruCache
 import android.widget.ImageView
-import kotlinx.coroutines.CoroutineScope
+import androidx.core.view.doOnAttach
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 object ImageLoader {
     private val cached: LruCache<String, Bitmap>
-    private val ioScope = CoroutineScope(Dispatchers.IO)
 
     init {
         val maxMemory = Runtime.getRuntime().maxMemory() / 1024
@@ -37,29 +38,39 @@ object ImageLoader {
             return
         }
 
-        ioScope.launch {
-            val source = ImageDecoder.createSource(context.contentResolver, uri)
-            val bitmap =
-                ImageDecoder.decodeBitmap(source) { decoder, info, _ ->
-                    val (originalWidth, originalHeight) = info.size.width to info.size.height
+        doOnAttach {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: throw IllegalStateException("View가 attach 되지 않았습니다.")
+            val imageView = it as ImageView
 
-                    if (originalWidth > maxWidth || originalHeight > maxHeight) {
-                        val aspectRatio = originalWidth.toFloat() / originalHeight.toFloat()
-
-                        if (originalWidth > originalHeight) {
-                            val scaledHeight = (maxWidth / aspectRatio).toInt()
-                            decoder.setTargetSize(maxWidth, scaledHeight)
-                        } else {
-                            val scaledWidth = (maxHeight * aspectRatio).toInt()
-                            decoder.setTargetSize(scaledWidth, maxHeight)
-                        }
-                    }
+            lifecycleOwner.lifecycleScope.launch {
+                val context = it.context
+                val bitmap = withContext(Dispatchers.IO) {
+                    decodeBitmap(
+                        source = ImageDecoder.createSource(context.contentResolver, uri),
+                        maxWidth = maxWidth,
+                        maxHeight = maxHeight,
+                    )
                 }
 
-            cached.put(uri.toString().substringAfterLast("/"), bitmap)
+                imageView.setImageBitmap(bitmap)
+            }
+        }
+    }
 
-            withContext(Dispatchers.Main) {
-                this@loadUri.setImageBitmap(bitmap)
+    private fun decodeBitmap(source: ImageDecoder.Source, maxWidth: Int, maxHeight: Int): Bitmap {
+        return ImageDecoder.decodeBitmap(source) { decoder, info, _ ->
+            val (originalWidth, originalHeight) = info.size.width to info.size.height
+
+            if (originalWidth > maxWidth || originalHeight > maxHeight) {
+                val aspectRatio = originalWidth.toFloat() / originalHeight.toFloat()
+
+                if (originalWidth > originalHeight) {
+                    val scaledHeight = (maxWidth / aspectRatio).toInt()
+                    decoder.setTargetSize(maxWidth, scaledHeight)
+                } else {
+                    val scaledWidth = (maxHeight * aspectRatio).toInt()
+                    decoder.setTargetSize(scaledWidth, maxHeight)
+                }
             }
         }
     }
