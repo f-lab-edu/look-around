d4056562a8b96e203059118ced060a3ea310ce3e
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-08-27 15:54:56 +0900

    build : feature 플러그인 제작 및 적용

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,45 +1,19 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.hiltPlugin)
+    id("lookaround.android.feature")
 }
 
 fun getApiKey(propertyKey: String): String = gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
 
 android {
     namespace = "kky.flab.lookaround.feature.home"
-    compileSdk = libs.versions.compileSdk.get().toInt()
 
     val naverApiKey = getApiKey("NAVER_API_KEY")
 
     defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-
         buildConfigField("String", "NAVER_API_KEY", naverApiKey)
         manifestPlaceholders["naverApiKey"] = naverApiKey
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 
     viewBinding {
@@ -48,23 +22,8 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:ui"))
-    implementation(project(":core:domain"))
-    implementation(project(":feature:recording"))
+    implementation(projects.feature.recording)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.fragment.ktx)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.service)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.hilt)
-    ksp(libs.hiltCompiler)
-
     implementation(libs.google.location)
 }
