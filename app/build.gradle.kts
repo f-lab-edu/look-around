6fd1d3c4c80110addc7d1bcdba77ddb2d54d6765
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-08-27 17:20:17 +0900

    build : 의존성 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,28 +26,5 @@ dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.data)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-    implementation(libs.hilt)
-    ksp(libs.hiltCompiler)
-
     implementation(libs.naver.map)
-
-//    implementation(libs.androidx.material3)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.ui.test.junit4)
-//    debugImplementation(libs.androidx.ui.tooling)
-//    debugImplementation(libs.androidx.ui.test.manifest)
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-
 }
