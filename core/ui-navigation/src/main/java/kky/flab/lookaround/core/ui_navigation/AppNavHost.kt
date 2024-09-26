2a974472bc402d5bf9eec6d242a530950a3fcd1d
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-09-26 22:42:57 +0900

    feat : NavHost 구현
    - 메인화면 NavHost 구현

---
+++ core/ui-navigation/src/main/java/kky/flab/lookaround/core/ui_navigation/AppNavHost.kt
@@ -0,0 +1,32 @@
+package kky.flab.lookaround.core.ui_navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kky.flab.lookaround.core.ui_navigation.route.MainRoute
+import kky.flab.lookaround.feature.home.HomeScreen
+import kky.flab.lookaround.feature.record.RecordScreen
+import kky.flab.lookaround.feature.setting.SettingScreen
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController(),
+    onShowSnackBar: (String) -> Unit,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = MainRoute.Home
+    ) {
+        composable<MainRoute.Home> {
+            HomeScreen()
+        }
+        composable<MainRoute.Record> {
+            RecordScreen()
+        }
+        composable<MainRoute.Setting> {
+            SettingScreen()
+        }
+    }
+}
