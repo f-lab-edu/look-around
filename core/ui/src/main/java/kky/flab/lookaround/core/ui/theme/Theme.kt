a3a1520fc3e54c374afd75765f0d10689ea5ad8f
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-09-26 22:32:03 +0900

    feat : Theme 수정
    - navigationbar, statusbar 컬러 지정 코드 추가

--- core/ui/src/main/java/kky/flab/lookaround/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/kky/flab/lookaround/core/ui/theme/Theme.kt
@@ -1,11 +1,13 @@
 package kky.flab.lookaround.core.ui.theme
 
 import android.app.Activity
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -24,7 +26,7 @@ val DarkColorScheme = darkColorScheme(
     background = Black,
     surface = DarkGray,
     onSurface = White,
-    onSurfaceVariant = Gray,
+    onSurfaceVariant = LightGray,
 )
 
 @Composable
@@ -32,14 +34,17 @@ fun LookaroundTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val colorScheme = if(darkTheme) DarkColorScheme else LightColorScheme
+
     val view = LocalView.current
     val activity = view.context as Activity
-    WindowCompat.getInsetsController(activity.window, activity.window.decorView).apply {
+    val window = activity.window
+    WindowCompat.getInsetsController(window, activity.window.decorView).apply {
         isAppearanceLightStatusBars = darkTheme.not()
         isAppearanceLightNavigationBars = darkTheme.not()
     }
-
-    val colorScheme = if(darkTheme) DarkColorScheme else LightColorScheme
+    window.statusBarColor = colorScheme.background.toArgb()
+    window.navigationBarColor = colorScheme.background.toArgb()
 
     MaterialTheme(colorScheme = colorScheme, content = content)
 }
