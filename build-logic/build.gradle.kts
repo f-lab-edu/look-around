2304e41d066a74827f497f93fc90d649d3a30844
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-11-01 20:25:35 +0900

    build : compose compiler metrics 설정

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
 }
--- build-logic/src/main/java/com/lookaround/app/ComposeAndroid.kt
+++ build-logic/src/main/java/com/lookaround/app/ComposeAndroid.kt
@@ -2,6 +2,8 @@ package com.lookaround.app
 
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 fun Project.configureComposeAndroid() {
     with(pluginManager) {
@@ -24,5 +26,9 @@ fun Project.configureComposeAndroid() {
         "implementation"(findLibrary("androidx-ui-tooling-preview"))
         "implementation"(findLibrary("androidx-ui-tooling"))
         "implementation"(findLibrary("androidx-compose-navigation"))
+    }
+
+    extensions.getByType<ComposeCompilerGradlePluginExtension>().apply {
+        reportsDestination.set(layout.buildDirectory.dir("compose_compiler"))
     }
 }
